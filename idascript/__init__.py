bd3689c802d8a66bb07c9cd8279ae3b34fb28fc6
RobinDavid <dev.robin.david@gmail.com>
2018-02-20 10:46:24 +0100

    rename env variable

--- idascript/__init__.py
+++ idascript/__init__.py
@@ -2,7 +2,7 @@ import sys
 import os
 from pathlib import Path
 
-IDAROOT_ENV = "IDAROOT"
+IDA_PATH_ENV = "IDA_PATH"
 BIN_NAME = "idat64.exe" if sys.platform == "win32" else "idat64"
 
 IDA_BINARY = None
@@ -10,9 +10,9 @@ IDA_BINARY = None
 
 def __check_environ() -> bool:
     global IDA_BINARY
-    if IDAROOT_ENV in os.environ:
-        if (Path(os.environ[IDAROOT_ENV]) / BIN_NAME).exists():
-            IDA_BINARY = (Path(os.environ[IDAROOT_ENV]) / BIN_NAME).absolute()
+    if IDA_PATH_ENV in os.environ:
+        if (Path(os.environ[IDA_PATH_ENV]) / BIN_NAME).exists():
+            IDA_BINARY = (Path(os.environ[IDA_PATH_ENV]) / BIN_NAME).absolute()
             return True
     return False
 
@@ -22,14 +22,14 @@ def __check_path() -> bool:
     if "PATH" in os.environ:
         for p in os.environ["PATH"].split(":"):
             if (Path(p) / BIN_NAME).exists():
-                IDA_BINARY = (Path(os.environ[IDAROOT_ENV]) / BIN_NAME).absolute()
+                IDA_BINARY = (Path(os.environ[IDA_PATH_ENV]) / BIN_NAME).absolute()
                 return True
     return False
 
 
 if not __check_environ():
     if not __check_path():
-        raise ImportError("IDA Pro executable not found, should be in $PATH or IDAROOT env variable")
+        raise ImportError("IDA Pro executable not found, should be in $PATH or IDA_PATH env variable")
 
 from idascript.ida import IDA, MultiIDA
 from idascript.utils import iter_binary_files
