346ec1841425a00cee40594b1f4691321e3273b4
RobinDavid <dev.robin.david@gmail.com>
2023-01-31 14:07:11 +0100

    update URL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='IDA Pro wrapper to launch script on binaries',
     author='Robin David',
     author_email='rdavid@quarkslab.com',
-    url='https://gitlab.qb/rdavid/idascript',
+    url='https://github.com/quarkslab/idascript',
     packages=['idascript'],
     install_requires=[
         'python-magic',
